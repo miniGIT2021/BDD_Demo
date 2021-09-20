f1f9706610fe5b0c3c1c4eca9e2b427c4fff4344
miniGIT2021 <90119195+miniGIT2021@users.noreply.github.com>
2021-09-20 18:38:27 +0530

    Update HelloWord.java

--- src/test/java/HelloWord.java
+++ src/test/java/HelloWord.java
@@ -1,6 +1,6 @@
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with Jenkin");
+        System.out.println("This is my first github commit integrating with Jenkins");
         System.out.println("This is commit into feature into master");
     }
 }
